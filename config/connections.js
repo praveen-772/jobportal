3ab3cf0f14204646a312041805ca745ab752f729
Praveen S Nair <praveen_772@yahoo.co.in>
2021-01-11 01:10:55 +0400

    Jobportal App in Heroku & Mongodb Alias Cloud

--- config/connections.js
+++ config/connections.js
@@ -5,6 +5,9 @@ module.exports.connect = function(done){
     const url = 'mongodb+srv://praveen_772:Praveenmongodb123@cluster0.z3wtz.mongodb.net/Jobportal?retryWrites=true&w=majority';
     const dbname = 'Jobportal'
 
+    // const url = 'mongodb://localhost:27017';
+    // const dbname = 'jobportal'
+
     mongoClient.connect(url,(err,data)=>{
         if (err) {  return done(err)    }
         else{
